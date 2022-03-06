983b51c67bccc9b3a53431f477aededa4e04fd40
Brian Jomo <98019316+BrianJomo@users.noreply.github.com>
2022-03-06 21:56:47 +0300

    It will add more variables in the js file.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 let malenames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 let femalenames = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 let dayofweek =["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+
+function calc() {
+
+var DD = document.getElementById("Day").value;
+var year = document.getElementById("Year").value;
+var MM = document.getElementById("Month").value;
+var male = document.getElementById("Man").checked;
+var female = document.getElementById("Woman").checked;
+
