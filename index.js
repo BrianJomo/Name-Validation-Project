2b9c8736dcf428abc964a57c7aa7fdf2309e7291
Brian Jomo <98019316+BrianJomo@users.noreply.github.com>
2022-03-06 21:55:24 +0300

    It will add variables in the js file.

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-
+let malenames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let femalenames = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+let dayofweek =["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
